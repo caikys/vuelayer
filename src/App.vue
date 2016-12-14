bbad935804d5e0509101b563c9bb54118f83df43
= <=>
2016-12-14 13:43:54 +0800

    first 第一次提交项目

---
+++ src/App.vue
@@ -0,0 +1,216 @@
+<template>
+  <div id="app">
+    <hello
+      :list="list" :title="title" :unit="num" 
+      @callback="getMsg"
+    ></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  data () {
+    return {
+      title: '列表',
+      list: [
+        {
+          name: '1'
+        },
+        {
+          name: '2'
+        },
+        {
+          name: '3'
+        },
+        {
+          name: '4'
+        },
+        {
+          name: '5'
+        },
+        {
+          name: '6'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        },
+        {
+          name: '7'
+        }
+      ],
+      willShow:true,
+      msg: '',
+      unit:''
+    }
+  },
+  methods: {
+    getMsg (msg) {
+      this.msg = msg
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <button @click.stop="fn()">{{unit}}{{msg}}</button>
+    <div class="pop" v-if="willShow"  @click="checktanchuceng">
+      <div class="list">
+        <div class="title">{{title}}</div>
+        <ul>
+          <li v-for="item in list" @click.stop="getMsg(item.name)">
+            {{item.name}}
+          </li>
+        </ul>  
+      </div>
+      
+    </div>
+  </div>
+</template>
+<style scoped src='../../static/sass/Hello.css'></style>
+
+<script>
+  export default {
+    name: 'hello',
+    props: {
+      unit: {   // 可选字段，有默认值
+        type: Number,
+        default:1
+      },
+      title: {
+        type: String,
+        required : true
+      },
+      willShow: {
+        type: Boolean
+        //required : true
+      },
+      list: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    },
+    data () {
+      return {
+        msg: ''
+      }
+    },
+    methods: {
+      getMsg (name) {
+        this.msg = name;
+        this.unit='';
+        this.$emit('callback', name)
+      },
+      checktanchuceng (){
+              if(this.willShow=true){
+                  this.willShow=false;
+              }
+          },
+      fn:function(){
+          if(this.willShow==true){
+              this.willShow=false;
+            }else{
+              this.willShow=true
+            }
+      }
+    }
+  }
+
+</script>
+
